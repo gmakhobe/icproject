534116ee1a19b3ea384b999ad8c5d4e44a1ffd11
Shaun <Shaunmlax@gmail.com>
2020-12-03 15:32:22 +0200

    indicator

--- resources/views/user/ideas/strategies-indicators.blade.php
+++ resources/views/user/ideas/strategies-indicators.blade.php
@@ -1,3 +1,161 @@
-<?php
-    echo "strategies-indicators page";
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+        <meta charset="utf-8">
+                <meta name="author" content="www.twitter.com/cheeriottis">
+
+        <link rel="stylesheet" href="css/core.css">
+                <title>indicators And Strategies</title>
+
+		<!-- description text that displays below the link in google search results -->
+		<meta name="description" content="Description of site" />
+    <!-- Start: Header Content -->
+    @include('/user/layout/header-style-1')
+    <!-- End: Header Content -->
+    <link rel="stylesheet" href="/assets1/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="/assets1/css/Article-List.css">
+    <link rel="stylesheet" href="/assets1/css/Profile-Card-1.css">
+    <link rel="stylesheet" href="/assets1/css/styles.css">
+    <link rel="stylesheet" href="/assets1/css/Vertical-Left-SideBar-by-Jigar-Mistry.css">
+    <link rel="stylesheet" href="/assets1/css/indicatorsAndStrategies.css">
+</head>
+
+<body style="background-color: white;">
+
+     <!-- Start: Vertical Left-SideBar by Jigar Mistry -->
+     @include('/user/layout/side-nav')
+     <!-- End: Vertical Left-SideBar by Jigar Mistry -->
+     <!-- Start: Top Navbar -->
+     @include('/user/layout/top-nav')
+     <!-- End: Top Navbar -->
+
+     <section class="page-wrap">
+
+
+<header> <!-- Defining the header section of the page with the appropriate tag -->
+
+    <div>
+        <h1>Indicators and strategies</h1>
+        <p>A subhead goes here.</p>
+    </div>
+
+    <nav> <!-- The nav link semantically marks your main site navigation -->
+        <ul>
+            <li><a href="#block1">My indicators</a></li>
+            <li><a href="#block2">Most Rated Indicators</a></li>
+            <li><a href="#block3">Most trending</a></li>
+    <li><a href="#block3">Analyse</a></li>
+        </ul>
+    </nav>
+
+
+</header>
+
+
+
+      <article id="block1" class="module-block">
+
+    <h2>Name of indivator</h2>
+
+    <img class="right" src="http://placehold.it/150" alt="" />
+<h3>
+            <a href="#">
+            Nature of indicator
+
+            </a>
+        </h3>
+    <p> Cupcake ipsum dolor. Sit amet sweet roll icing cupcake ice cream sugar plum. Danish I love biscuit carrot cake tart jelly beans candy. Chupa chups chocolate bar I love pie ice cream brownie gummi bears. Soufflé cookie I love gummi bears faworki sweet sweet tart croissant. Muffin bonbon danish wafer candy macaroon I love dessert. Muffin pudding danish chupa chups. Halvah lollipop applicake icing. Brownie I love cupcake. I love gingerbread cheesecake gummi bears powder candy toffee icing. Pastry jelly beans candy canes dragée croissant I love halvah jujubes. Cotton candy icing wafer caramels applicake.</p>
+
+    <ul class="social">
+
+        <li><a href="#" class="action-button shadow animate red">ADD</a></li>
+
+        <li> <a href="#" class="action-button shadow animate green">REVIEWS</a></li>
+            <li> <a href="#" class="action-button shadow animate blue">RELATION</a></li>
+        <li> </li>
+
+    </ul>
+
+</article>
+  <article id="block1" class="module-block">
+
+    <h2>Name of indicator</h2>
+
+    <img class="right" src="http://placehold.it/150" alt="" />
+<h3>
+            <a href="#">
+            Nature of indicator
+
+            </a>
+        </h3>
+    <p style="color:white"> Cupcake ipsum dolor. Sit amet sweet roll icing cupcake ice cream sugar plum. Danish I love biscuit carrot cake tart jelly beans candy. Chupa chups chocolate bar I love pie ice cream brownie gummi bears. Soufflé cookie I love gummi bears faworki sweet sweet tart croissant. Muffin bonbon danish wafer candy macaroon I love dessert. Muffin pudding danish chupa chups. Halvah lollipop applicake icing. Brownie I love cupcake. I love gingerbread cheesecake gummi bears powder candy toffee icing. Pastry jelly beans candy canes dragée croissant I love halvah jujubes. Cotton candy icing wafer caramels applicake.</p>
+
+    <ul class="social">
+
+        <li><a href="#" class="action-button shadow animate red">ADD</a></li>
+
+        <li> <a href="#" class="action-button shadow animate green">REVIEWS</a></li>
+            <li> <a href="#" class="action-button shadow animate blue">RELATION</a></li>
+        <li> </li>
+
+    </ul>
+
+</article>
+
+  <article id="block1" class="module-block">
+
+    <h2>Name of indicator</h2>
+
+    <img class="right" src="http://placehold.it/150" alt="" />
+<h3>
+            <a href="#">
+            Nature of indicator
+
+            </a>
+        </h3>
+    <p style="color:white"> Cupcake ipsum dolor. Sit amet sweet roll icing cupcake ice cream sugar plum. Danish I love biscuit carrot cake tart jelly beans candy. Chupa chups chocolate bar I love pie ice cream brownie gummi bears. Soufflé cookie I love gummi bears faworki sweet sweet tart croissant. Muffin bonbon danish wafer candy macaroon I love dessert. Muffin pudding danish chupa chups. Halvah lollipop applicake icing. Brownie I love cupcake. I love gingerbread cheesecake gummi bears powder candy toffee icing. Pastry jelly beans candy canes dragée croissant I love halvah jujubes. Cotton candy icing wafer caramels applicake.</p>
+
+    <ul class="social">
+
+        <li><a href="#" class="action-button shadow animate red">ADD</a></li>
+
+        <li> <a href="#" class="action-button shadow animate green">REVIEWS</a></li>
+            <li> <a href="#" class="action-button shadow animate blue">RELATION</a></li>
+        <li> </li>
+
+    </ul>
+
+</article>
+
+
+
+
+
+
+
+</section><!-- page-wrap -->
+
+
+<footer>
+
+<section class="page-wrap">
+
+
+
+
+
+
+</section>
+
+<p>Copyright 2012 - <a href="mailto:">Your Name</a></p>
+<p class="up"><a href="#">Go Up ⇪</a></p>
+
+</footer>
+
+     <!-- Start: Header Content -->
+     @include('/user/layout/footer-style-1')
+     <!-- End: Header Content -->
+</body>
+
+</html>
