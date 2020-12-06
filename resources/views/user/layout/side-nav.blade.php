b8ff8c628ca10944d73798c95806096c58b0030e
Shaun <Shaunmlax@gmail.com>
2020-12-06 11:04:52 +0200

    navigation bar

--- resources/views/user/layout/side-nav.blade.php
+++ resources/views/user/layout/side-nav.blade.php
@@ -12,6 +12,86 @@
 
     <link rel="stylesheet" href="css/main.css">
 
+<script>
+$(function () {
+
+'use strict';
+
+(function () {
+//부트스트랩 햄버거 클릭시 사이드바 on off
+
+  var aside = $('.side-nav'),
+
+      showAsideBtn = $('.show-side-btn'),
+
+      contents = $('#contents');
+
+  showAsideBtn.on("click", function () {
+
+    $("#" + $(this).data('show')).toggleClass('show-side-nav');
+
+    contents.toggleClass('margin');
+
+  });
+
+  if ($(window).width() <= 767) {
+
+    aside.addClass('show-side-nav');
+
+  }
+  $(window).on('resize', function () {
+
+    if ($(window).width() > 767) {
+
+      aside.removeClass('show-side-nav');
+
+    }
+
+  });
+
+  // dropdown menu in the side nav
+  var slideNavDropdown = $('.side-nav-dropdown');
+
+  $('.side-nav .categories li').on('click', function () {
+
+    $(this).toggleClass('lol').siblings().removeClass('lol');
+
+    if ($(this).hasClass('lol')) {
+
+      $(this).find('.side-nav-dropdown').slideToggle('fast');
+
+      $(this).siblings().find('.side-nav-dropdown').slideUp('fast');
+
+    } else {
+
+      $(this).find('.side-nav-dropdown').slideUp('fast');
+
+    }
+
+  });
+
+  $('.side-nav .close-aside').on('click', function () {
+
+    $('#' + $(this).data('close')).addClass('show-side-nav');
+
+    contents.removeClass('margin');
+
+  });
+
+}());
+
+
+
+
+
+console.log(Chart.defaults.global);
+
+
+
+
+});
+</script>
+
 
 
 
@@ -153,6 +233,7 @@
 
                 }
 
+
             </script>
 
             <div class="d-none d-sm-block topbar-divider"></div>
@@ -228,13 +309,10 @@
                 </a>
                 </li>
                 <li class="dropdown">
-<<<<<<< HEAD
 
 
               <li class="dropdown">
 
-=======
->>>>>>> d2fb04d703a618221abf4a770f4b40a6d98a0982
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My profile <span class="caret"></span></a>
                 <ul class="dropdown-menu">
                   <li><a href="#"><i class="fa fa-user-o fw"></i> My account</a></li>
