636220a1db1f0484f0771d7dd0fb35fee25a5997
Shaun <Shaunmlax@gmail.com>
2020-12-17 11:50:22 +0200

    Fix awards and rawmat

--- resources/views/user/rawmate/rawmate.blade.php
+++ resources/views/user/rawmate/rawmate.blade.php
@@ -482,9 +482,7 @@ console.log(Chart.defaults.global);
     @include('/user/layout/footer-style-1')
     <!-- End: Header Content -->
     <!-- End: Dark footer with social media icons -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     <script src="/assets1/js/bs-init.js"></script>
     <script src="/assets1/js/Data-Table-with-Search-Sort-Filter-and-Zoom-using-TableSorter.js"></script>
     <script src="/assets1/js/theme.js"></script>
