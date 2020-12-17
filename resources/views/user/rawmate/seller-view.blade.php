3e85294611f91c44f36f82a93f21e596b64cb987
Shaun <Shaunmlax@gmail.com>
2020-12-17 09:47:08 +0200

    Rawmat and Seller

--- resources/views/user/rawmate/seller-view.blade.php
+++ resources/views/user/rawmate/seller-view.blade.php
@@ -21,47 +21,123 @@
     <link rel="stylesheet" href="/assets1/css/styles.css">
     <link rel="stylesheet" href="/assets1/css/Vertical-Left-SideBar-by-Jigar-Mistry.css">
     <link rel="stylesheet" href="/assets1/css/newSideNav.css">
+    <link rel="stylesheet" href="/assets1/css/Seller.css">
+
 </head>
 
-<body style="background-color: rgb(33,31,31);margin-top: 2px;height: 201px;">
+<body>
 
     <!-- Start: Vertical Left-SideBar by Jigar Mistry -->
     @include('/user/layout/side-nav')
     <!-- End: Vertical Left-SideBar by Jigar Mistry -->
     <!-- Start: Top Navbar -->
-
+    <br>
     <!-- End: Top Navbar -->
-
-    <div class="m-2">
-        <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-10">
-
-                <div class="jumbotron my-bg-color-grey">
-                    <h1 class="my-text-color-white">Company Name</h1>
+    <div class="code--css" style="height: 150px; color:white" align="center">
+                    <div style="padding: 2%">
+                    <h2 class="my-text-color-white">Shaun gustavo</h2>
                     <p class="my-text-color-white">Description</p>
                     <p class="my-text-color-white">Start Date</p>
-                  </div>
-
-                <div class="row">
-                    <div class="col-md-3 card my-bg-color-grey p-3">
-                        <div class="card-body">
-                            <a href="/user/rawmate/product/name/id">
-                                <img class="my-rawmate-main-thumbnail" src="https://via.placeholder.com/250x350"
-                                    alt="Raw mate image" />
-                                <p class="my-text-color-white"><strong>Product Name</strong>
-                                </p>
-                                <p class="my-text-color-white">Seller Name</p>
-                                <p class="my-text-color-white"><strong>Amount</strong></p>
-                            </a>
-                        </div>
-                    </div>
                 </div>
+                </div>
+                <br>
 
+ <div id="pattern" class="pattern">
+  	<div class="c">
+			<div class="main">
+            <div class="ex-1 example">
+                <ul>
+                    <li><i class="fa fa-desktop"></i><span>Management</span></li>
+                    <li class="active"><i class="fa fa-newspaper-o"></i><span>News</span></li>
+                    <li><i class="fa fa-cubes"></i><span>Specification</span></li>
+                    <li><i class="fa fa-server"></i><span>stock</span></li>
+                    <li><i class="fa fa-line-chart"></i><span>analyze</span></li>
+
+                </ul>
+                </div>
             </div>
-            <div class="col-md-1"></div>
-        </div>
-    </div>
+
+			</div>
+			<div class="c2">
+            <nav class="Navi">
+            <ul>
+                <li><a href="">MARKET RESPONSE</a></li>
+                <li><a href="">COMPANY POLICY</a></li>
+                <li><a href="">COMPANY RECORDS</a></li>
+
+            </ul>
+            </nav>
+			</div>
+			<div class="c3">
+            <div id="wrapper">
+                    <div id="content">
+                    <h1>Lightbox Thumbnail Gallery</h1>
+
+                    <div class="photos">
+                        <ul class="clearfix">
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/963800-New-App-design -->
+                        <li><a href="https://i.imgur.com/xcJnVXQ.png" rel="lightbox" title="Designed by <a href='http://dribbble.com/shots/963800-New-App-design' target='_blank'>Natalia Berowska</a>"><img src="https://i.imgur.com/uHfYjsK.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/963671-Navigation -->
+                        <li><a href="https://i.imgur.com/aIhKIUa.png" rel="lightbox" title="Designed by <a href='http://dribbble.com/shots/963671-Navigation' target='_blank'>Matt D. Smith</a>"><img src="https://i.imgur.com/EOoQQmW.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/962966-Share -->
+                        <li><a href="https://i.imgur.com/RkS7wPJ.png" rel="lightbox" title="Designed by <a href='http://dribbble.com/shots/962966-Share' target='_blank'>Nicholas Swanson</a>"><img src="https://i.imgur.com/VPTqPdp.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/961333-Profile-Screen-WIP -->
+                        <li><a href="https://i.imgur.com/WnPWszE.png" rel="lightbox" title="Designed by <a href='http://dribbble.com/shots/961333-Profile-Screen-WIP' target='_blank'>Alex Sharpz</a>"><img src="https://i.imgur.com/G7EKgOQ.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/961314-Chat-IOS-App -->
+                        <li><a href="https://i.imgur.com/A5i8Bh8.png" rel="lightbox" title="Designed by <a href='http://dribbble.com/shots/961314-Chat-IOS-App' target='_blank'>Daniela Alves</a>"><img src="https://i.imgur.com/myR8vnh.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/958102-Traveler-3-0-Favorites -->
+                        <li><a href="https://i.imgur.com/Tt4lSvw.png" rel="lightbox" title="Designed by <a href='http://dribbble.com/shots/958102-Traveler-3-0-Favorites' target='_blank'>Lynette Liwanag</a>"><img src="https://i.imgur.com/RD57vEE.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/960585-Twitter-Client-UI-Design -->
+                        <li><a href="https://i.imgur.com/ylpbRQi.png" rel="lightbox" title="Designed by <a href='http://dribbble.com/shots/960585-Twitter-Client-UI-Design' target='_blank'>Mike Hince</a>"><img src="https://i.imgur.com/qdQEJdy.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/957175-Share-Section -->
+                        <li><a href="https://i.imgur.com/tFNQYQw.png" rel="lightbox" title="Designed by <a href='http://dribbble.com/shots/957175-Share-Section' target='_blank'>Evan Kimia</a>"><img src="https://i.imgur.com/Kjpl4Ux.png" width="150" height="150"></a></li>
+                        </ul>
+                    </div>
+
+                    <h1>Secondary Gallery</h1>
+                    <div class="photos">
+                        <ul class="clearfix">
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/965861-Gas-Heater-Ios-Icon -->
+                        <li><a href="https://i.imgur.com/y10RjXf.png" rel="2ndlightbox" title="Designed by Edu Torres"><img src="https://i.imgur.com/6hXASFX.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/964877-PassBox-iOS-Icon -->
+                        <li><a href="https://i.imgur.com/zxeSqXy.png" rel="2ndlightbox" title="Designed by Takahashi Alex"><img src="https://i.imgur.com/toRiLo3.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/965043-DINO-DOMINION-iOS-Icon -->
+                        <li><a href="https://i.imgur.com/h1QBn3o.png" rel="2ndlightbox" title="Designed by Gregory Kozhemyak"><img src="https://i.imgur.com/Y8Za1a5.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/961772-Tagger-icon -->
+                        <li><a href="https://i.imgur.com/MZe8Klr.png" rel="2ndlightbox" title="Designed by Alex Bender"><img src="https://i.imgur.com/rnSzelo.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/957195-Air-Hockey -->
+                        <li><a href="https://i.imgur.com/qAIJ1sc.png" rel="2ndlightbox" title="Designed by Alex Bender"><img src="https://i.imgur.com/hhCoryO.png" width="150" height="150"></a></li>
+
+                        <!-- ATTRIBUTION: http://dribbble.com/shots/955207-iOS-App-Icon-Design-Reading-Rainbow -->
+                        <li><a href="https://i.imgur.com/ddulKwU.png" rel="2ndlightbox" title="Designed by The Iconfactory"><img src="https://i.imgur.com/5T1tKRq.png" width="150" height="150"></a></li>
+                        </ul>
+                    </div>
+                    </div> <!-- /end #content -->
+
+                    <footer id="credits">
+                    <p>Lightbox effect generated via <a href="http://leandrovieira.com/projects/jquery/lightbox/" target="_blank">jQuery Lightbox Plugin</a>.</p>
+                    </footer>
+                </div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
 
     <script>
 
