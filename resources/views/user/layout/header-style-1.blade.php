11720fd543f7a42f0dc52418d9ba6c1b15293b47
Shaun <Shaunmlax@gmail.com>
2021-01-10 21:27:50 +0200

    script

--- resources/views/user/layout/header-style-1.blade.php
+++ resources/views/user/layout/header-style-1.blade.php
@@ -1,98 +1,14 @@
-<!-- <meta charset="utf-8">
+
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 <title>{{ $Title }} - {{ $Name }}</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css">
-<link rel="stylesheet" href="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.css"> -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
 <link rel="stylesheet" href="css/main.css">
+
+<script src="/assets1/js/side-nav.js"></script>
 <!-- side nav -->
-<script>
-$(function () {
-
-'use strict';
-
-(function () {
-//부트스트랩 햄버거 클릭시 사이드바 on off
-
-  var aside = $('.side-nav'),
-
-      showAsideBtn = $('.show-side-btn'),
-
-      contents = $('#contents');
-
-  showAsideBtn.on("click", function () {
-
-    $("#" + $(this).data('show')).toggleClass('show-side-nav');
-
-    contents.toggleClass('margin');
-
-  });
-
-  if ($(window).width() <= 767) {
-
-    aside.addClass('show-side-nav');
-
-  }
-  $(window).on('resize', function () {
-
-    if ($(window).width() > 767) {
-
-      aside.removeClass('show-side-nav');
-
-    }
-
-  });
-
-  // dropdown menu in the side nav
-  var slideNavDropdown = $('.side-nav-dropdown');
-
-  $('.side-nav .categories li').on('click', function () {
-
-    $(this).toggleClass('lol').siblings().removeClass('lol');
-
-    if ($(this).hasClass('lol')) {
-
-      $(this).find('.side-nav-dropdown').slideToggle('fast');
-
-      $(this).siblings().find('.side-nav-dropdown').slideUp('fast');
-
-    } else {
-
-      $(this).find('.side-nav-dropdown').slideUp('fast');
-
-    }
-
-  });
-
-  $('.side-nav .close-aside').on('click', function () {
-
-    $('#' + $(this).data('close')).addClass('show-side-nav');
-
-    contents.removeClass('margin');
-
-  });
-
-}());
-
-
-
-
-
-console.log(Chart.defaults.global);
-
-
-
-
-});
-</script>
 <!-- Start: Javascript
 <script src="/js/validator.js"></script>
 <!-- <script src="/js/navbar-tools.js"></script> -->
